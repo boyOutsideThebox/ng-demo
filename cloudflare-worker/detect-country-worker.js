65991410e325bb5f4167bc81fe8d154c88d244f9
Arghaya Das13 <adas13@lenovo.com>
2024-07-29 23:37:36 +0530

    fixing route path

--- cloudflare-worker/detect-country-worker.js
+++ cloudflare-worker/detect-country-worker.js
@@ -1,24 +1,14 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  // Fetch the original response from the origin server
-  let response = await fetch(request)
-
-  // Only modify HTML content
-  if (response.headers.get("Content-Type")?.includes("text/html")) {
-    // Read the response body
-    let html = await response.text()
-
-    // Modify the HTML - example: insert country code
-    const countryCode = request.headers.get('cf-ipcountry')
-    html = html.replace('<!--COUNTRY_PLACEHOLDER-->', countryCode)
-
-    // Create a new response with the modified HTML
-    response = new Response(html, response)
-    response.headers.set('Content-Type', 'text/html')
-  }
-
-  return response
-}
+export default {
+    async fetch(request, env, ctx) {
+      const response = await fetch(request);
+  
+      const rewriter = new HTMLRewriter().on('input#countryCode', {
+        element(element) {
+          const countryCode = request.headers.get('cf-ipcountry');
+          element.setAttribute('value', countryCode || 'UNKNOWN');
+        },
+      });
+  
+      return rewriter.transform(response);
+    },
+  };
