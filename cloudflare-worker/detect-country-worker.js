c9e659989d39554a0af0872f527dc140db93c8b3
Arghaya Das13 <adas13@lenovo.com>
2024-07-30 06:05:35 +0530

    changing it from docs folder

--- cloudflare-worker/detect-country-worker.js
+++ cloudflare-worker/detect-country-worker.js
@@ -1,14 +1,15 @@
 export default {
-    async fetch(request, env, ctx) {
-      const response = await fetch(request);
-  
-      const rewriter = new HTMLRewriter().on('input#countryCode', {
-        element(element) {
-          const countryCode = request.headers.get('cf-ipcountry');
-          element.setAttribute('value', countryCode || 'UNKNOWN');
-        },
-      });
-  
-      return rewriter.transform(response);
-    },
-  };
+  async fetch(request, env, ctx) {
+    const response = await fetch(request);
+    const countryCode = request.headers.get("cf-ipcountry");
+    console.log(`Country code: ${countryCode}`);
+    const rewriter = new HTMLRewriter().on('input#countryCode', {
+      element(element) {
+        const countryCode = request.headers.get('cf-ipcountry');
+        element.setAttribute('value', countryCode || 'UNKNOWN');
+      },
+    });
+
+    return rewriter.transform(response);
+  },
+};
